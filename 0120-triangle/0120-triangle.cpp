a41517ef023a2c61af45b5a8b760795161b24ec1
Himanshu Singh <work.himanshu16@gmail.com>
2024-12-18 05:40:38 +0530

    Time: 0 ms (100%), Space: 12.4 MB (33.05%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        // Start from the second last row and move upwards
         for (int i = n - 2; i >= 0; i--) {
             for (int j = 0; j < triangle[i].size(); j++) {
                 int minSum = min(triangle[i + 1][j], triangle[i + 1][j + 1]);
