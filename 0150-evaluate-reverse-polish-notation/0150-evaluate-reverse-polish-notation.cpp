677201ab49605e9a9b1a00b1b78ac2f5806ccd6d
Himanshu Singh <work.himanshu16@gmail.com>
2024-12-21 14:24:39 +0530

    Time: 3 ms (38.54%), Space: 17.4 MB (12.66%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int top;
+    int f(vector<string>& tokens){
+        string s=tokens[top--];
+        if (isdigit(s[0]) || s.size()>1)
+            return stoi(s);
+        int n2=f(tokens);
+        int n1=f(tokens);
+    //    cout<<n1<<", "<<n2<<s<<endl;
+        switch(s[0]){
+            case '+': return n1+n2; 
+            case '-': return n1-n2; 
+            case '*': return n1*n2; 
+            case '/': return n1/n2; 
+        }
+        return 0;
+    }
+    int evalRPN(vector<string>& tokens) {  
+        top=tokens.size()-1;     
+        return f(tokens);
+    }
+};
