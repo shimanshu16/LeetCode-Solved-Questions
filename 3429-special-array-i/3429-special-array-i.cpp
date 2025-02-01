4d52a737fde854d9d912806dc5d87870cc4218da
Himanshu Singh <work.himanshu16@gmail.com>
2025-02-01 19:54:56 +0530

    Time: 0 ms (100%), Space: 27.7 MB (25%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.cpp
+++ 3429-special-array-i/3429-special-array-i.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool isArraySpecial(vector<int>& nums) {
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums[i] % 2 == nums[i - 1] % 2) {
+        for(int i = 0; i < nums.size() - 1; i++) {
+            if (nums[i] % 2 == nums[i + 1] % 2) {
                 return false;
             }
         }
