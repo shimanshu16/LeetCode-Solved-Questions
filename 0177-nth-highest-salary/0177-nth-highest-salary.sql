f8d4f2ef3f05bddce1bfd19404c4d060bb381571
Himanshu Singh <work.himanshu16@gmail.com>
2025-04-15 11:54:41 +0530

    Time: 934 ms (32.66%), Space: 0B (100%) - LeetHub

---
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -0,0 +1,13 @@
+CREATE FUNCTION getNthHighestSalary(N INT) 
+RETURNS INT
+BEGIN
+    RETURN (
+        SELECT DISTINCT salary
+        FROM Employee e1
+        WHERE (
+            SELECT COUNT(DISTINCT salary)
+            FROM Employee e2
+            WHERE e2.salary >= e1.salary
+        ) = N
+    );
+END;
