456aa4fce088161fd3da94bef94c2ef5fa5f9ac3
Himanshu Singh <work.himanshu16@gmail.com>
2025-05-16 23:28:51 +0530

    Time: 0 ms (100%), Space: 30.2 MB (26.09%) - LeetHub

--- 3143-longest-unequal-adjacent-groups-subsequence-i/3143-longest-unequal-adjacent-groups-subsequence-i.cpp
+++ 3143-longest-unequal-adjacent-groups-subsequence-i/3143-longest-unequal-adjacent-groups-subsequence-i.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
-    static vector<string> getLongestSubsequence(vector<string>& words, vector<int>& groups) {
-        const int n=groups.size();
-        int prev=groups[0];
-        int len=1;
-        for(int i=1, j=1; i<n; i++){
-            while(i<n && prev==groups[i])
-                i++;
-            if (i<n) {
-                words[j++]=words[i];
-                len++;
-                prev=groups[i];
+    vector<string> getLongestSubsequence(vector<string>& words,
+                                         vector<int>& groups) {
+        vector<string> result;
+        result.reserve(words.size());    
+        result.push_back(std::move(words[0]));
+        int last = groups[0];
+        for (int i = 1; i < (int)words.size(); ++i) {
+            if (groups[i] != last) {
+                result.push_back(std::move(words[i]));
+                last = groups[i];
             }
         }
-        words.resize(len);
-        return words;
+        return result;
     }
 };
